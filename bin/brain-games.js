c7827f672042059feeef680c7b1d1542e62080b5
Denys Voloshyn <denys.voloshyn@ria.com>
2020-05-21 22:38:11 +0300

    Add ThirdStep: add cli module

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { askUserName } from "../src/cli.js";
 
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name || 'stranger'}!`);
+askUserName();
