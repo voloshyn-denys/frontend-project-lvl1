cc83b27b9e5541addeddfc070a16b0896473cdaa
Denys Voloshyn <voloshyn.denys@gmail.com>
2020-05-27 12:54:25 +0300

    add SixthStep: add calc game

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,6 +1,32 @@
-import { runEngine, setupPlayer } from '../index.js';
+import { generateRandomNumber, runEngine, setupPlayer } from '../index.js';
 
-const generateCalcPair = () => [0, 0];
+const generateCalcPair = () => {
+  const operations = ['+', '-', '*'];
+
+  const firstNumber = generateRandomNumber(20);
+  const secondNumber = generateRandomNumber(20);
+  const operator = operations[generateRandomNumber(3)];
+
+  const question = `${firstNumber} ${operator} ${secondNumber}`;
+  let answer = null;
+
+  switch (operator) {
+    case '+':
+      answer = firstNumber + secondNumber;
+      break;
+
+    case '-':
+      answer = firstNumber - secondNumber;
+      break;
+
+    default:
+      answer = firstNumber * secondNumber;
+      break;
+  }
+  // or const answer = eval(question);
+
+  return [question, String(answer)];
+};
 
 const game = () => {
   const playerName = setupPlayer('What is the result of the expression?');
